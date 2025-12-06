3a08f86004e354a109d282c8428816bde340cecf
NaroEngineering <128151988+NaroEngineering@users.noreply.github.com>
2025-12-06 08:56:47 -0500

    Initial Commit

---
+++ PromptPal/PromptPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptPalApp.swift
+//  PromptPal
+//
+//  Created by Max Caro on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptPalTests/PromptPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromptPalTests.swift
+//  PromptPalTests
+//
+//  Created by Max Caro on 12/6/25.
+//
+
+import Testing
+@testable import PromptPal
+
+struct PromptPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
